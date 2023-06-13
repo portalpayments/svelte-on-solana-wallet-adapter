f01873b04d972f250d2cf85c596a6b10d3eb1ec5
Mike MacCana <mike.maccana@gmail.com>
2023-06-13 19:48:07 +0100

    Remove unused exports per README file

--- packages/ui/src/lib/index.ts
+++ packages/ui/src/lib/index.ts
@@ -1,7 +1,4 @@
 export { default as ConnectionProvider } from "./ConnectionProvider.svelte";
-export { default as ChooseWalletAdapterButton } from "./ChooseWalletAdapterButton.svelte";
-export { default as WalletConnectButton } from "./WalletConnectButton.svelte";
-export { default as WalletModal } from "./WalletModal.svelte";
-export { default as WalletMultiButton } from "./WalletMultiButton.svelte";
 export { default as WalletProvider } from "./WalletProvider.svelte";
+export { default as WalletMultiButton } from "./WalletMultiButton.svelte";
 export * from "./workSpace";
