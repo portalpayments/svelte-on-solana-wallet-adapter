c631c86ace982807b92243145d1f8aa8819888a6
Mike MacCana <mike.maccana@gmail.com>
2023-06-14 12:32:25 +0100

    Explicitly export functions rather than export *

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,5 @@
-export * from "./errors";
-export * from "./localStorage";
-export * from "./walletStore";
+// TODO: not even sure all of these are needed
+// But the old code exported '*'
+export { WalletNotSelectedError } from "./errors";
+export { getLocalStorage, setLocalStorage } from "./localStorage";
+export { initialize, walletStore, type WalletStore } from "./walletStore";
